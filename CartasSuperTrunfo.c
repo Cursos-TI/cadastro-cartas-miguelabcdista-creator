a7bcbd3232b3c9393403371c1b190805a738a452
miguelabcdista-creator <miguelabcdista@gmail.com>
2026-06-05 17:54:18 +0000

    fiz o desafio nivel novato, usando: char; int; float e printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,87 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+int mai() {
+   //Variaveis da carta 1
+   char estado1;
+   char codigo1[4];
+   char cidade1[50];
+   int populaçao1;
+   float area1;
+   float pib1;
+   int pontosTuristicos1;
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+   //Variaveis da carta 2
+   char estado2;
+   char codigo2[4];
+   char cidade2[50];
+   int populaçao2;
+   float area2;
+   float pib2;
+   int pontosTuristicos2;
 
-  // Área para entrada de dados
+   // Cadastro carta 1
+   printf("=== Cadastro da carta 1 ===\n");
 
-  // Área para exibição dos dados da cidade
+   printf("Estado (A a H): ");
+   scanf(" %c", &estado1);
 
-return 0;
-} 
+   printf("Codigo da Carta (ex: A01): ");
+   scanf("%s", codigo1);
+
+   printf("Nome da Cidade: ");
+   scanf(" %s", cidade1);
+
+   printf("Populaçao: ");
+   scanf("%d", &populaçao1);
+
+   printf("Area (km²): ");
+   scanf("%f", &area1);
+
+   printf("PIB (em bilhoes de reais): ");
+   scanf("%f", &pib1);
+
+   printf("Numero de pontos turisticos: ");
+   scanf("%d", &pontosTuristicos1);
+
+   //Cadastro de carta 2
+   printf("\n=== Cadastro da Carta 2 ===\n");
+
+   printf("Estado (A a H): ");
+   scanf(" %c", &estado2);
+
+   printf("Codigo da Carta (ex: A01): ");
+   scanf("%s", codigo2);
+
+   printf("Nome da Cidade: ");
+   scanf(" %s", cidade2);
+
+   printf("Populaçao: ");
+   scanf("%d", &populaçao2);
+
+   printf("Area (km²): ");
+   scanf("%f", &area2);
+
+   printf("PIB (em bilhoes de reais): ");
+   scanf("%f", &pib2);
+
+   printf("Numero de pontos turisticos: ");
+   scanf("%d", &pontosTuristicos2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
